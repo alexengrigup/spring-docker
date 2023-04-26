3a52b6ce81ac33125999c4fcdef23ddd44a92bd7
Grig Alex <alexengrigdev@gmail.com>
2023-04-26 20:18:58 +0300

    Remove @EurekaNamespace

--- producer-service/src/main/java/dev/alexengrig/spring/docker/producer/controller/ProducerController.java
+++ producer-service/src/main/java/dev/alexengrig/spring/docker/producer/controller/ProducerController.java
@@ -1,9 +1,6 @@
 package dev.alexengrig.spring.docker.producer.controller;
 
-import brave.Span;
-import com.netflix.discovery.EurekaNamespace;
 import dev.alexengrig.spring.docker.producer.service.ProducerService;
-import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +14,6 @@ public class ProducerController {
     private final ProducerService service;
 
     @GetMapping
-    @EurekaNamespace
     public String produce() {
         return service.produce();
     }
